50a61fe61bfd3102bfcd702e5d3ef6d46b42aebd
Jahadul-Rakib <rakibdiu2015@gmail.com>
2023-01-31 18:03:42 +0600

    add

---
+++ src/data_type_enum_test.go
@@ -0,0 +1,36 @@
+package src
+
+import (
+	"log"
+	"testing"
+)
+
+/*----------------------------------------------------*/
+type ColorType string
+
+const (
+	SOLID ColorType = "Solid"
+	DEEP  ColorType = "Deep"
+)
+
+/*----------------------------------------------------*/
+type Size uint8
+
+const (
+	small      Size = 0
+	medium     Size = 1
+	large      Size = 2
+	extraLarge Size = 3
+)
+
+/*----------------------------------------------------*/
+
+func TestDataTypeEnum(t *testing.T) {
+	log.Println(SOLID)
+	log.Println(DEEP)
+
+	log.Println(small)
+	log.Println(medium)
+	log.Println(large)
+	log.Println(extraLarge)
+}
